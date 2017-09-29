d6b7039453e2bd2c398824c6155eb6a8d3e9dc39
Tran Trung Kien <kientt13.7@gmail.com>
2017-09-29 16:13:18 +0700

    fix error

--- server.js
+++ server.js
@@ -7,60 +7,57 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/shipdata";
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+var coll;
+
 app.get('/', function (req,res){
 	res.render('index.jade');
 });
 
-app.post('/quicksearch', urlencodedParser, function (req,res){
-	MongoClient.connect(url, function (err, db) {
-        let ship_imo = req.body.imo;
-        let checked = req.body.checked;
-        console.log(req.body);
-        if (err) throw err;
-        if(checked === "false"){
-            db.collection("ship2").find({'IMO': {'$regex': ship_imo}}).limit(4).toArray(function(err, result) {
-                if (err) throw err;
-                res.send(result);
-                res.end();
-                db.close();
-            });
-        }
-        else{
-            db.collection("ship2").find({'IMO': ship_imo}).limit(4).toArray(function(err, result) {
-                if (err) throw err;
-                res.send(result);
-                res.end();
-                db.close();
-            });
-        }
-    });
+MongoClient.connect(url, function(err,db){
+    if (err) throw err;
+    coll = db.collection('ship2');
+})
+
+app.post('/quicksearch', urlencodedParser, function (req,res){	
+    let ship_imo = req.body.imo;
+    let checked = req.body.checked;
+    if(checked === "false"){
+        coll.find({'IMO': {'$regex': ship_imo}}).limit(4).toArray(function(err, result) {
+            if (err) throw err;
+            res.send(result);
+            res.end();
+        });
+    }
+    else{
+        coll.find({'IMO': ship_imo}).limit(4).toArray(function(err, result) {
+            if (err) throw err;
+            res.send(result);
+            res.end();
+        });
+    }
 });
 
-app.post('/search-result', urlencodedParser, function (req,res){
-    MongoClient.connect(url, function (err, db) {
-        let ship_imo = req.body.IMO;
-        if (err) throw err;
-        db.collection("ship2").find({'IMO': {'$regex': ship_imo}}).toArray(function(err, results) {
+app.get('/search-result', urlencodedParser, function (req,res){
+    
+        let ship_imo = req.query.IMO;
+        
+        coll.find({'IMO': {'$regex': ship_imo}}).toArray(function(err, results) {
             if (err) throw err;
             res.render('kq.jade', {results});
             res.end();
-            db.close();
-        });
-    });
+        });    
 });
 
-app.post('/ship/view-detail', urlencodedParser, function (req, res) {
-    MongoClient.connect(url, function(err, db) {
-        let ship_imo = req.body.IMO;
-        let ship_name = req.body.Name;
-        if (err) throw err;
-        db.collection("ship2").findOne({'IMO': ship_imo, 'Name': ship_name},function(err, result) {
+app.get('/ship/view-detail', urlencodedParser, function (req, res) {
+    
+        let ship_imo = req.query.IMO;
+        let ship_name = req.query.Name;
+        
+        coll.findOne({'IMO': ship_imo, 'Name': ship_name},function(err, result) {
             if (err) throw err;
             res.render('detail.jade',{result});
             res.end();
-            db.close();
-        });
-    });
+        });    
 });
 
 
